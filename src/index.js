66b6120d99971709c859d47c9b5a3d2cc161caa2
Yishu Fang <yishu.fang@mioying.com>
2017-09-02 19:29:54 +0800

    Add Google Analytics.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import createHistory from 'history/createHashHistory';
+
+import ReactGA from 'react-ga';
 
 import App from "./App/App";
 
 import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 
+ReactGA.initialize('UA-101258772-2');
+
+function logPageView(location) {
+  console.log(location.pathname, location.search);
+  ReactGA.set({ page: location.pathname + location.search });
+  ReactGA.pageview(location.pathname + location.search);
+}
+
+const history = createHistory();
+history.listen((location, action) => {
+  logPageView(location);
+});
+logPageView(history.location);
 
 ReactDOM.render(
     (
-        <HashRouter>
+        <Router history={history}>
             <App />
-        </HashRouter>
+        </Router>
     ),
     document.getElementById("root")
 );
-
